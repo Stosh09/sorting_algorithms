791e8090116f1bd047cbd7082cd40c2643a229fe
Stosh09 <stoshochieng@gmail.com>
2023-09-25 03:03:35 +0300

    Created our first header file with our first function prototype

---
+++ sort.h
@@ -0,0 +1,39 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
+/**
+ * enum boolean - Enumeration of Boolean values.
+ * @false: Equals 0.
+ * @true: Equals 1.
+ */
+typedef enum boolean
+{
+	false = 0,
+	true
+} boolean;
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+} listint_t;
+
+/* Sorting algoritms */
+void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
